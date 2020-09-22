aa180963ff3cb229d8f0dfd9a91165b8c60e8c88
Luis Augusto <hello@luis.codes>
2020-09-21 21:19:31 -0700

    added event signup date to header

--- components/HeroImage.vue
+++ components/HeroImage.vue
@@ -8,7 +8,13 @@
     <v-row align="center" justify="center">
       <v-col class="text-center" sm="10" md="8">
         <template v-if="eventItem.title">
-          <div class="text-overline">Now open for signups!</div>
+          <div
+            class="text-overline"
+            v-if="new Date(eventItem['signup-date']) > Date.now()"
+          >
+            Signups open {{ getDate(eventItem['signup-date']) }}
+          </div>
+          <div v-else class="text-overline">Now open for signups!</div>
           <h1 class="text-md-h1 text-h2">{{ eventItem.title }}</h1>
           <div class="text-md-h5 mb-4 text-h6">
             {{ getDate(eventItem['start-date']) }} -
