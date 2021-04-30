1251d73ff33baa93dbe6094aaacca498cb7d4cd7
Luis Augusto <hello@luis.app>
2021-04-30 13:22:14 -0700

    Dependency updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -116,5 +116,9 @@ export default {
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
    */
-  build: {},
+  build: {
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    },
+  },
 }
